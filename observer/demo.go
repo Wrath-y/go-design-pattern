67b6983068b58f4932c3306b9d486b8ec671378f
Wrath-y <805655528@qq.com>
2023-03-21 18:21:43 +0800

    feat: 优化策略模式实现

--- observer/demo.go
+++ observer/demo.go
@@ -1,55 +1,42 @@
 package observer
 
-import "strconv"
-
-type Subject interface {
-	SetSign(_ string)
-	GetSign() string
-	AddObserver(_ Observer)
-}
+import "fmt"
 
 type Observer interface {
-	GetSign(i int)
-	SetSubject(_ Subject)
+	Update()
 }
 
-type Teacher struct {
-	contentOfTheCourse string
-	Students           []Observer
-}
-type Student struct {
-	Teacher Subject
+type Subject struct {
+	observers []Observer
 }
 
-func (t *Teacher) SetSign(contentOfTheCourse string) {
-	t.contentOfTheCourse = contentOfTheCourse
-	println("老师:" + contentOfTheCourse)
-	println("老师:我刚刚说了什么你们复述一遍")
-	t.NotifyStudents()
+func (s *Subject) Attach(o Observer) {
+	s.observers = append(s.observers, o)
 }
 
-func (t *Teacher) GetSign() string {
-	return t.contentOfTheCourse
-}
-
-func (t *Teacher) AddObserver(s Observer) {
-	if t.Students == nil {
-		t.Students = make([]Observer, 0)
-	}
-	t.Students = append(t.Students, s)
-}
-
-func (t *Teacher) NotifyStudents() {
-	for i, stu := range t.Students {
-		stu.GetSign(i)
+func (s *Subject) Detach(o Observer) {
+	for i, observer := range s.observers {
+		if observer == o {
+			s.observers = append(s.observers[:i], s.observers[i+1:]...)
+			break
+		}
 	}
 }
 
-func (s *Student) GetSign(i int) {
-	println("学生" + strconv.Itoa(i) + ":" + s.Teacher.GetSign())
+func (s *Subject) Notify() {
+	for _, observer := range s.observers {
+		observer.Update()
+	}
 }
 
-func (s *Student) SetSubject(t Subject) {
-	s.Teacher = t
-	s.Teacher.AddObserver(s)
+type ConcreteObserverA struct{}
+
+func (o *ConcreteObserverA) Update() {
+	fmt.Println("ConcreteObserverA has been notified")
+}
+
+type ConcreteObserverB struct{}
+
+func (o *ConcreteObserverB) Update() {
+	fmt.Println("ConcreteObserverB has been notified")
 }
--- observer/run.go
+++ observer/run.go
@@ -1,14 +1,16 @@
 package observer
 
+// Run
+// 在示例中，我们定义了一个 Subject 结构体和一个 Observer 接口。具体观察者类（例如 ConcreteObserverA 和 ConcreteObserverB）实现了 Update() 方法，并注册到被观察者对象中。
+// 当被观察者状态发生改变时，它将调用自己的 Notify() 方法来通知所有注册过的观察者对象。每个观察者对象都实现了 Update() 方法，并提供了自己的更新方法。
 func Run() {
-	teacher := new(Teacher)
+	subject := &Subject{}
 
-	student1 := new(Student)
-	student1.SetSubject(teacher)
-	student2 := new(Student)
-	student2.SetSubject(teacher)
-	student3 := new(Student)
-	student3.SetSubject(teacher)
+	observerA := &ConcreteObserverA{}
+	subject.Attach(observerA)
 
-	teacher.SetSign("体育老师请假了，这节课我来上")
+	observerB := &ConcreteObserverB{}
+	subject.Attach(observerB)
+
+	subject.Notify()
 }
